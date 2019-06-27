cf7ee59fdd993900cd936c2fb2d95ed88e9ea017
Erik Yo <erikyo83@gmail.com>
2019-06-27 12:59:25 +0200

    woocommerce sidebar restyling + better category page

--- functions.php
+++ functions.php
@@ -3,4 +3,3 @@
 require_once( get_stylesheet_directory() . '/inc/clean.php' );
 require_once( get_stylesheet_directory() . '/inc/masonry.php' );
 require_once( get_stylesheet_directory() . '/inc/enqueue-scripts.php' );
-
