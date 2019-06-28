376013e7f1dc0ceb73d8aaa6d839eaef30092e65
Erik Yo <erikyo83@gmail.com>
2019-06-28 19:20:53 +0200

    woocommerce image fix

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -13,22 +13,22 @@ add_action( 'after_setup_theme', 'moduchild_theme_setup' );
 /**
  * load the cookie policy banner
  */
-function modul_r_theme_scripts() {
 
+function modul_r_theme_scripts() {
 	// Register and Enqueue
 	wp_enqueue_script( 'jquery' );
+
 	wp_register_script( 'scripts-vendors', get_template_directory_uri() . "/assets/dist/js/vendor-scripts.js" , array('jquery'), null, true );
 	wp_enqueue_script( 'scripts-vendors' );
 
-	wp_register_script( 'scripts', get_stylesheet_directory_uri() . "/assets/dist/js/scripts.js" , array('scripts-vendor'), null, true );
-	wp_enqueue_script( 'scripts' );
+	wp_register_script( 'scripts-main', get_stylesheet_directory_uri() . "/assets/dist/js/scripts.js" , array('scripts-vendors'), null, true );
+	wp_enqueue_script( 'scripts-main' );
 
 	$cookie_args = array(
 		'message' => sprintf( __( 'This website uses cookies to improve user experience, memorizing your preferences and monitorizing site funcionality. Check out our <a href="%s">Cookie Policy</a>', 'modul-r-child'), get_site_url() . '/cookie-policy' ),
 		'button' => __('Ok, I understand', 'modul-r-child' )
 	);
 
-	wp_localize_script( 'scripts', 'args', $cookie_args );
+	wp_localize_script( 'scripts-main', 'args', $cookie_args );
 }
-
 add_action( 'wp_enqueue_scripts', 'modul_r_theme_scripts' );
