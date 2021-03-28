33b3f77bc2c61083a90e67b45f2012d78b61140b
Erik Yo <erikyo83@gmail.com>
2021-03-28 16:39:08 +0200

    modular default variables enabled also for child theme

--- functions.php
+++ functions.php
@@ -1,5 +1,19 @@
 <?php
 
+$modul_r_defaults = array(
+	'colors' => array(
+		'primary' => '',
+		'secondary' => '',
+		'header' => '',
+		'white' => '#ffffff',
+		'white-smoke' => '#f3f3f3',
+		'gray-light' => '#e3e3e3',
+		'gray' => '#888888',
+		'gray-dark' => '#4e4e4e',
+		'black' => '#222222',
+	),
+);
+
 require_once( get_stylesheet_directory() . '/inc/mimetypes.php' );
 require_once( get_stylesheet_directory() . '/inc/clean.php' );
 require_once( get_stylesheet_directory() . '/inc/masonry.php' );
