f2fc03f761da8ca6a0531aaa634acdeb217f98e7
Erik Yo <erikyo83@gmail.com>
2021-04-02 00:47:06 +0200

    Remove the prefix before the category title

--- inc/clean.php
+++ inc/clean.php
@@ -12,3 +12,20 @@ remove_action( 'wp_head', 'rsd_link' ); // Remove EditURI link
 
 // Add Filters
 //add_filter('show_admin_bar', '__return_false'); // Disable Admin Bar for All Users Except for Administrators
+
+
+// Remove the prefix before the category title
+add_filter( 'get_the_archive_title', function ($title) {
+	if ( is_category() ) {
+		$title = single_cat_title( '', false );
+	} elseif ( is_tag() ) {
+		$title = single_tag_title( '', false );
+	} elseif ( is_author() ) {
+		$title = '<span class="vcard">' . get_the_author() . '</span>' ;
+	} elseif ( is_tax() ) { //for custom post types
+		$title = sprintf( __( '%1$s' ), single_term_title( '', false ) );
+	} elseif (is_post_type_archive()) {
+		$title = post_type_archive_title( '', false );
+	}
+	return $title;
+});
