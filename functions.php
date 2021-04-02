c3605c4b011717af49167c3ca5ee98b3a9148b2d
Erik Yo <erikyo83@gmail.com>
2021-04-02 12:32:11 +0200

    removed unwanted dev code

--- functions.php
+++ functions.php
@@ -27,23 +27,3 @@ require_once( get_stylesheet_directory() . '/inc/mimetypes.php' );
 require_once( get_stylesheet_directory() . '/inc/clean.php' );
 require_once( get_stylesheet_directory() . '/inc/masonry.php' );
 require_once( get_stylesheet_directory() . '/inc/enqueue-scripts.php' );
-
-function modul_r_archive_image( $class = null ) {
-	// Check if Thumbnail exists
-	if ( has_post_thumbnail() ) : ?>
-		<div class="entry-image hero fullpage-hero interactive<?php echo ' ' . esc_attr($class); ?>">
-			<div class="entry-image">
-				<?php the_post_thumbnail( 'modul-r-fullwidth', array( 'class' => 'fit-image wp-post-image' ) ); ?>
-			</div>
-			<div class="hero-title text-center">
-				<span class="category-payoff background-text text-outline">MADE IN ITALY</span>
-				<?php the_archive_title( '<h1 class="page-title main-width has-title-color">', '</h1>' ); ?>
-				<?php if (is_author()) {
-					printf('<p>%s</p>', esc_html(get_the_author_meta( 'description' )) );
-				} else {
-					the_archive_description();
-				} ?>
-			</div>
-		</div>
-	<?php endif;
-}
